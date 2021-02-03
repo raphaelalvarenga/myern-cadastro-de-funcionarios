6f66fb40de7a705acd3491f6fb0c33531fe1e849
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2021-02-03 20:12:25 -0300

    [Services] Criados todos os metodos dos servicos. Mas alguns apenas existem e nao possuem implementacoes. Serao implementados de acordo com os modulos do CRUD.

--- frontend/src/classes/services.class.ts
+++ frontend/src/classes/services.class.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 import util from "../util/util";
+import { Cargo } from "./cargo.class";
+import { Funcionario } from "./funcionario.class";
 
 export class Service {
     getFuncionarios() {
         return axios.get(`${util.api}/funcionarios`);
     }
+
+    getFuncionarioById(id: number) { }
+
+    postFuncionario(newFuncionario: Funcionario) { }
+
+    putFuncionario(funcionario: Funcionario) { }
 
     deleteFuncionario(id: number) {
         return axios.delete(`${util.api}/funcionarios/${id}`);
@@ -13,6 +21,12 @@ export class Service {
     getCargos() {
         return axios.get(`${util.api}/cargos`);
     }
+
+    getCargoById(id: number) { }
+
+    postCargo(newCargo: Cargo) { }
+
+    putCargo(cargo: Cargo) { }
 
     deleteCargo(id: number) {
         return axios.delete(`${util.api}/cargos/${id}`);
