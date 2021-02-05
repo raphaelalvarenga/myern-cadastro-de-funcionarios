ddd62cba51da78e6f21eb255092e9f2c54d0182c
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2021-02-04 23:14:11 -0300

    Mais correcoes nos models.

--- backend/src/models/cargo.model.ts
+++ backend/src/models/cargo.model.ts
@@ -1,6 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../util/connection";
 
-export const CargoModel = sequelize.define("cargo", {
+export const CargoModel = sequelize.define("Cargo", {
     descricao: DataTypes.STRING
 });
